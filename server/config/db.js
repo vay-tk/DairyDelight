e40530449d13ad969b032decc83daf86509958f5
vay-tk <vaytk001@gmail.com>
2025-12-05 10:27:06 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://vaytk001:vaytk001@cluster0.5izk0mj.mongodb.net/dairy-delight");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
